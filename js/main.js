b7cbc632237b978461c473dde3c9d0994b5991b2
Thomas Davis <thomasalwyndavis@gmail.com>
2013-09-17 00:57:36 +1000

    Asd

--- js/main.js
+++ js/main.js
@@ -5,24 +5,19 @@ require.config({
     jquery: 'libs/jquery/jquery-min',
     underscore: 'libs/underscore/underscore', // https://github.com/amdjs
     backbone: 'libs/backbone/backbone', // https://github.com/amdjs
-    prettyprint: 'libs/prettyprint/prettyprint',
-    qtip: 'libs/qtip2/jquery.qtip.min',
     mustache: 'libs/mustache/mustache',
     epiceditor: 'libs/epiceditor/epiceditor',
     bootstrap: 'libs/bootstrap/bootstrap',
     // APIe
-    modal: 'libs/modal/modal',
     form: 'libs/form/form',
     // Require.js plugins
     text: 'libs/require/text',
     // jquery plugins
-    autogrow : 'libs/autogrow/autogrow',
     // External services
 
     // Just a short cut so we can put our html outside the js dir
     // When you have HTML/CSS designers this aids in keeping them out of the js directory
     templates: '../templates',
-    legal: '../legal'
   }
 
 });
